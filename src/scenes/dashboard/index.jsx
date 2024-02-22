a0a24b0ce4e608818c5c2bdbca6b44c6abe22d74
John Korio <koriojohn59@gmail.com>
2024-02-22 22:23:41 +0300

    data grid, header,sidebar

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,6 +1,13 @@
-
-const Dashboard = ()=> {
-    return <div>Dashboard</div>
-}
+import { Box } from "@mui/material";
+import Header from "../../components/Headers";
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+      <Box alignItems="center" justifyContent="space-between" display="flex">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard"></Header>
+      </Box>
+    </Box>
+  );
+};
 
 export default Dashboard;
